8c22a3cb398bfdd839cf50f3a8c15698e34b9bc3
rhit-olingejj <olingejj@rose-hulman.edu>
2023-03-20 14:23:17 -0400

    all

--- FunctionApp1/Function1.cs
+++ FunctionApp1/Function1.cs
@@ -9,7 +9,7 @@ namespace FunctionApp1
     public class Function1
     {
         [FunctionName("Function1")]
-        public void Run([BlobTrigger("example/{name}", Connection = "")]Stream myBlob, string name, ILogger log)
+        public void Run([BlobTrigger("example/{name}", Connection = "DefaultEndpointsProtocol=https;AccountName=testfunctions1;AccountKey=XwvE5rQQV36SKXAP0jU8sNgYJUVVRQWdeMSGE/4CGyFZodpzECXsSrDEAust2Cqx2lstVNHB4QNj+AStxqROvA==;EndpointSuffix=core.windows.net")]Stream myBlob, string name, ILogger log)
         {
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
         }
